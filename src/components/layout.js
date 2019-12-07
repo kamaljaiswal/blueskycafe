3c71a3820761e7fc43993089db83abc21f2bfafd
crolla97 <cristianocrolla@gmail.com>
2019-12-07 01:39:05 +0000

    Added project files

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import '../styles/main.scss'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
