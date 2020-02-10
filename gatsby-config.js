dd2ffcb693afd201c292efee1f36ba36bed76c6a
crolla97 <cristianocrolla@gmail.com>
2020-02-10 14:42:44 +0000

    Added sitemap and changed the favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,12 @@ module.exports = {
   siteMetadata: {
     title: `Dantes`,
     description: `Dantes is a family run Italian restaurant in the heart of Colinton, Edinburgh. Our fully-licensed restaurant offers a great range of traditional Italian cuisine and a fantastic à la carte menu.`,
-    author: `@crollacreations`,
+    author: `crollacreations`,
+    siteUrl: `https://www.dantesrestaurant.co.uk`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -24,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
