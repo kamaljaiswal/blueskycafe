8f0a377a60920be743f12e6ee64f1f17737336dc
Kamal Jaiswal <Kamal@Kamals-MacBook-Pro.local>
2020-08-05 13:31:29 +0530

    Contact Us/ Opening hrs

--- src/components/header.js
+++ src/components/header.js
@@ -3,37 +3,52 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 
 const Header = ({ siteTitle, parentCallback }) => {
-  const [isOpen, changeState] = useState(false);
-  const lineColor1 = isOpen ? 'white line line1' : 'line line1';
-  const lineColor2 = isOpen ? 'white line line2' : 'line line2';
-  const lineColor3 = isOpen ? 'white line line3' : 'line line3';
+  const [isOpen, changeState] = useState(false)
+  const lineColor1 = isOpen ? "white line line1" : "line line1"
+  const lineColor2 = isOpen ? "white line line2" : "line line2"
+  const lineColor3 = isOpen ? "white line line3" : "line line3"
 
   return (
-  <div>
-    <div className="mobileMenu">
-      <header>
-        <Link to="/" className="logo">{siteTitle}</Link>
-      </header>
-      <div className="burgerMenu" onClick={(() => {
-        let newValue = !isOpen
-        changeState(!isOpen);
-        parentCallback(newValue);
-      })}>
-        <span className={lineColor1}></span>
-        <span className={lineColor2}></span>
-        <span className={lineColor3}></span>
+    <div>
+      <div className="mobileMenu">
+        <header>
+          <Link to="/" className="logo">
+            {siteTitle}
+          </Link>
+        </header>
+        <div
+          className="burgerMenu"
+          onClick={() => {
+            let newValue = !isOpen
+            changeState(!isOpen)
+            parentCallback(newValue)
+          }}
+        >
+          <span className={lineColor1}></span>
+          <span className={lineColor2}></span>
+          <span className={lineColor3}></span>
+        </div>
+      </div>
+      <div className="desktopMenu">
+        <nav>
+          <span>
+            <Link to="/about">About</Link>
+          </span>
+          {/* <span><Link to="/menus/takeaway">Menu</Link></span> */}
+          <span>
+            <Link to="/" className="logo">
+              {siteTitle}
+            </Link>
+          </span>
+          <span>
+            <Link to="/gallery">Gallery</Link>
+          </span>
+          <span>
+            <Link to="/contact">Contact</Link>
+          </span>
+        </nav>
       </div>
     </div>
-    <div className="desktopMenu">
-      <nav>
-        <span><Link to="/about">About</Link></span>
-        <span><Link to="/menus/takeaway">Menu</Link></span>
-        <span><Link to="/" className="logo">{siteTitle}</Link></span>
-        <span><Link to="/gallery">Gallery</Link></span>
-        <span><Link to="/contact">Contact</Link></span>
-      </nav>
-    </div>
-  </div>
   )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,45 +1,42 @@
 import React from "react"
 
-import LeafletMap from '../components/leafletmap'
+import LeafletMap from "../components/leafletmap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const ContactPage = () => {
-
   return (
-  <Layout>
-    <SEO title="Contact"/>
-    <div className="contactPage">
-    <section className="contactContainer">
-      <div className="contact">
-        <h2 className="contact__title">Contact Us</h2>
-        <span className="contact__body">0131 441 7427</span>
-        <span className="contactFooter__body">sandro@dantesrestaurant.co.uk</span>
-        <span className="contactFooter__body">fabio@dantesrestaurant.co.uk</span>
+    <Layout>
+      <SEO title="Contact" />
+      <div className="contactPage">
+        <section className="contactContainer">
+          <div className="contact">
+            <h2 className="contact__title">Contact Us</h2>
+            <span className="contact__body">+91-3322522958</span>
+            {/* <span className="contactFooter__body">
+              sandro@dantesrestaurant.co.uk
+            </span>
+            <span className="contactFooter__body">
+              fabio@dantesrestaurant.co.uk
+            </span> */}
+          </div>
+          <div className="table">
+            <h2 className="table__title">Opening Hours</h2>
+            <ul className="table__days">
+              <li>Mon-Fri</li>
+              <li>Sat-Sun</li>
+            </ul>
+            <ul className="table__hours">
+              <li>08:00 - 22:00</li>
+              <li>08:00 - 22:00</li>
+            </ul>
+          </div>
+        </section>
+        <section className="contactMap">
+          {typeof window !== "undefined" && <LeafletMap />}
+        </section>
       </div>
-      <div className="table">
-        <h2 className="table__title">Opening Hours</h2>
-        <ul className="table__days">
-          <li>Mon</li>
-          <li>Tue-Thu</li>
-          <li>Fri-Sat</li>
-          <li>Sun</li>
-        </ul>
-        <ul className="table__hours">
-          <li>Closed</li>
-          <li>17:30 - 21:00</li>
-          <li>12:00 - 14:00 <br/> 17:30 - 21:00</li>
-          <li>12:00 - 14:00 <br/> 17:00 - 20:00</li>
-        </ul>
-      </div>
-    </section>
-    <section className="contactMap">
-      {typeof window !== 'undefined' &&
-        <LeafletMap/>
-      }
-    </section>
-    </div>
-  </Layout>
+    </Layout>
   )
 }
 
