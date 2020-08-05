637ca22b6d001ae75d1c7f8c5a8611bb21c0b2ca
Kamal Jaiswal <Kamal@Kamals-MacBook-Pro.local>
2020-08-05 16:46:35 +0530

    conetent updated

--- src/components/sideDrawer.js
+++ src/components/sideDrawer.js
@@ -1,21 +1,36 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
 const SideDrawer = ({ menuOpen }) => {
-  const menuClass = menuOpen ? 'drawer open' : 'drawer';
-
+  const menuClass = menuOpen ? "drawer open" : "drawer"
 
   return (
     <nav className={menuClass}>
       <ul className="sideNavbar">
-        <li><Link to='/' activeClassName="active" >Home</Link></li>
-        <li><Link to='/menus/takeaway' activeClassName="active">Menu</Link></li>
-        <li><Link to='/gallery' activeClassName="active">Gallery</Link></li>
-        <li><Link to='/about' activeClassName="active">About</Link></li>
-        <li><Link to='/contact' activeClassName="active">Contact</Link></li>
+        <li>
+          <Link to="/" activeClassName="active">
+            Home
+          </Link>
+        </li>
+        {/* <li><Link to='/menus/takeaway' activeClassName="active">Menu</Link></li> */}
+        <li>
+          <Link to="/gallery" activeClassName="active">
+            Gallery
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" activeClassName="active">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" activeClassName="active">
+            Contact
+          </Link>
+        </li>
       </ul>
-      <span className="logo">Dantes</span>
-      <span className="address">48-50 Bridge Road, Colinton</span>
+      <span className="logo">BlueSkyCafe</span>
+      <span className="address">Sudder St, New Market Area </span>
     </nav>
   )
 }
